6e44583fb7f4548ab88bd153bb81b56c39dcefd9
Daniele Monaco <daniele.monaco@capgemini.com>
2024-02-23 23:37:49 +0100

    pannello admin

--- main.py
+++ main.py
@@ -46,6 +46,46 @@ def save_color_to_storage(data):
     blob.upload_from_string(json_string, content_type='application/json')
     print("colori salvati")
 
+def save_mats_to_storage(data):
+    print("entro save mats")
+    bucket_name='sitolamiere.appspot.com'
+    filename='materiali.json'
+    storage_client = init_storage_client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(filename)
+     # Aggiunto il blocco try-except per il debug della serializzazione JSON
+    try:
+        json_string = json.dumps(data)
+        print("Dati serializzati con successo:", json_string)  # Stampa per debug
+    except TypeError as e:
+        print("Errore nella serializzazione dei dati:", e)
+        # Qui puoi decidere come gestire l'errore, ad esempio ritornare un errore al client
+        return {"error": f"Impossibile serializzare i dati: {e}"}
+
+    # Procede con il caricamento dei dati serializzati su Google Cloud Storage
+    blob.upload_from_string(json_string, content_type='application/json')
+    print("materiali salvati")
+   
+def save_profile_to_storage(data):
+    print("entro save profile")
+    bucket_name='sitolamiere.appspot.com'
+    filename='profili.json'
+    storage_client = init_storage_client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(filename)
+     # Aggiunto il blocco try-except per il debug della serializzazione JSON
+    try:
+        json_string = json.dumps(data)
+        print("Dati serializzati con successo:", json_string)  # Stampa per debug
+    except TypeError as e:
+        print("Errore nella serializzazione dei dati:", e)
+        # Qui puoi decidere come gestire l'errore, ad esempio ritornare un errore al client
+        return {"error": f"Impossibile serializzare i dati: {e}"}
+
+    # Procede con il caricamento dei dati serializzati su Google Cloud Storage
+    blob.upload_from_string(json_string, content_type='application/json')
+    print("profili salvati")
+
 def load_color_from_storage(bucket_name='sitolamiere.appspot.com', filename='colori.json'):
     print("entro load")
     storage_client = init_storage_client()
@@ -63,6 +103,22 @@ def load_profile_from_storage(bucket_name='sitolamiere.appspot.com', filename='p
     json_data = json.loads(blob.download_as_string(client=None))
     print("profili caricati")
     return json_data
+
+def load_mats_from_storage(bucket_name='sitolamiere.appspot.com', filename='materiali.json'):
+    storage_client = init_storage_client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(filename)
+    json_data = json.loads(blob.download_as_string())
+    return json_data
+
+def upload_image_to_gcs(bucket_name, image_content, image_filename):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob('img/' + image_filename)  # 'img/' è la cartella nel tuo bucket
+    blob.upload_from_string(image_content, content_type='image/png')
+    blob.make_public()
+    return blob.public_url
+
 
 @app.route('/')
 def homepage():
@@ -128,48 +184,32 @@ def save_profile():
     try:
         data = request.json
         imageSrc = data.get('imageSrc', '')
-
-        # Verifica e divide la stringa imageSrc
+        
+        
         if ',' in imageSrc:
             image_data = imageSrc.split(',')[1]
             image = base64.b64decode(image_data)
         else:
-            # Gestisce il caso in cui la stringa imageSrc non è nel formato atteso
             return jsonify({'error': 'Formato immagine non valido'}), 400
-    
-    
-        for key, value in data.items():
-            print(f'{key}: {value}')
-        # Controlla se tutti i campi richiesti sono presenti e non vuoti
-        required_fields = ['imageSrc', 'profileName', 'containerInputQuoteValues', 'listaSviluppoValues', 'containerInputImmaginiValues']
-       
-        for field in required_fields:
-            if field not in data or not data[field]:
-                return jsonify({'error': f'Campo mancante o vuoto: {field}'}), 400
 
-        json_file_path = 'data/profili.json'
         profile_name = data['profileName']
-        idProfile = profile_name+"_new"
-        # Carica il file JSON dei profili per controllare se il nome esiste già
-        with open(json_file_path, 'r') as json_file:
-            profili = json.load(json_file)
-            if any(profilo['id'] == profile_name for profilo in profili['profili']):
-                # Il nome del profilo esiste già
-                return jsonify({'error': 'Attenzione! Nome profilo già esistente.'}), 400
+        idProfile = profile_name + "_new"
+        json_file_path = 'profili.json'  # Il nome del file nel bucket
+        image_filename = f'{profile_name}.png'
+        # Carica il file JSON dei profili da GCP
+        profili = load_color_from_storage(filename=json_file_path)
 
-        # Procedi con il salvataggio dell'immagine
+        if any(profilo['id'] == profile_name for profilo in profili['profili']):
+            return jsonify({'error': 'Attenzione! Nome profilo già esistente.'}), 400
+
+        # Procedi con il salvataggio dell'immagine su bucket
         image_data = data['imageSrc'].split(',')[1]
         image = base64.b64decode(image_data)
-        image_filename = f'{profile_name}.png'
-        image_filepath = os.path.join('static', 'img', image_filename)
-
-        # Salva l'immagine
-        with open(image_filepath, 'wb') as f:
-            f.write(image)
-
+        image_url = upload_image_to_gcs('sitolamiere.appspot.com', image, image_filename)
         
         input_boxes = []
         quote_values = data['containerInputQuoteValues']
+        print(quote_values)
         for i in range(0, len(quote_values), 2):
             input_box = {
                 "id": f"lato{i // 2 + 1}",  # Genera un ID univoco basato sull'indice
@@ -183,10 +223,10 @@ def save_profile():
         img_values = data['containerInputImmaginiValues']
         for i in range(0, len(img_values), 2):
             if i == 0:
-                url = "static/img/freccia_interna.png"
+                url = "https://storage.googleapis.com/sitolamiere.appspot.com/img/freccia_interna.png"
                 id = "1"
             else:
-                url = "static/img/freccia_esterna.png"
+                url = "https://storage.googleapis.com/sitolamiere.appspot.com/img/freccia_esterna.png"
                 id = "2"
             vern_box = {
                 "id": f"scelta{id}",  # Genera un ID univoco basato sull'indice
@@ -196,33 +236,32 @@ def save_profile():
             }
             vern_boxes.append(vern_box)
             
-        scalature = []
+        scalature = {}
         scala_values = data['listaSviluppoValues']
-        scalatura = {"id": idProfile}  # Inizia con l'ID del profilo
+        
         # Aggiungi le coppie chiave-valore per ogni spessore
         for spessore, valore in scala_values.items():
-            scalatura[spessore] = valore
+            scalature[spessore] = int(valore)
 
-        # Aggiungi l'oggetto scalatura alla lista delle scalature
-        scalature.append(scalatura)
         
-        with open(json_file_path, 'r+') as json_file:
-            profili = json.load(json_file)
-            
-            nuovo_profilo = {
-                "id": idProfile,  # Genera un ID univoco
-                "imageUrl": image_filepath.replace("\\", "/"),  # Sostituisci "\\" con "/"
-                "inputBoxes": input_boxes , # Aggiungi la sezione "inputBoxes"
-                "vernBoxes" :vern_boxes,
-                "Scalature": scalature
-            }   
-            profili['profili'].append(nuovo_profilo)
-            json_file.seek(0)  # Riposiziona all'inizio del file
-            json.dump(profili, json_file, indent=4)
-
+        
+        # Prepara i dati del nuovo profilo
+        nuovo_profilo = {
+            "id": idProfile,
+            "imageUrl": image_url,  # Adatta se necessario per immagini caricate su GCS
+            "inputBoxes": input_boxes,
+            "vernBoxes": vern_boxes,
+            "scalature": scalature
+        }
+        
+        # Aggiungi il nuovo profilo ai profili esistenti e salva su GCS
+        profili['profili'].append(nuovo_profilo)
+        save_profile_to_storage(profili)
+        
+        
         return jsonify({'message': 'Profilo salvato con successo'})
     except Exception as e:
-        print("Si è verificato un errore: ", str(e))
+        print("Si è verificato un errore:", str(e))
         return jsonify({'error': 'Si è verificato un errore interno del server'}), 500
     
     
@@ -309,32 +348,112 @@ def delete_color():
         return jsonify({'error': str(e)}), 500
 
 
+
 @app.route('/modify_profile', methods=['POST'])
 def modify_profile():
     data = request.json
     profile_id = data['id']
     new_scalatures = data['scalature']
 
-    # Percorso al file JSON dei profili
-    json_file_path = 'data/profili.json'
+    # Nome del bucket e del file nel bucket
+    bucket_name = 'sitolamiere.appspot.com'
+    file_name = 'profili.json'
+    
+    # Inizializza il client di Google Cloud Storage
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    
+    # Leggi il contenuto esistente del file JSON dal bucket
+    json_data = json.loads(blob.download_as_string(client=None))
+    
+    # Ottieni la lista dei profili, se esiste, altrimenti usa una lista vuota
+    profili = json_data.get('profili', [])
+    
+    # Trova il profilo da modificare e aggiorna le scalature
+    for profilo in profili:
+        if profilo['id'] == profile_id:
+            profilo['scalature'] = new_scalatures
+            break
 
-    # Carica il JSON dei profili
-    with open(json_file_path, 'r+') as file:
-        data = json.load(file)  # Carica il contenuto del file JSON in un dizionario
-        profili = data.get('profili', [])  # Ottieni la lista dei profili, se esiste, altrimenti usa una lista vuota
-
-        # Trova il profilo da modificare e aggiorna le scalature
-        for profilo in profili:
-            if profilo['id'] == profile_id:
-                profilo['scalature'] = new_scalatures
-                break
-
-        # Riporta il cursore all'inizio del file e sovrascrivi con i nuovi dati
-        file.seek(0)
-        json.dump(data, file, indent=4)  # Assicurati di scrivere l'oggetto 'data' completo, non solo 'profili'
-        file.truncate()
-
+    # Sovrascrivi il file nel bucket con i nuovi dati
+    blob.upload_from_string(json.dumps(json_data, indent=4), content_type='application/json')
+    
     return jsonify({'message': f'Scalature per il profilo {profile_id} modificate con successo!'})
+
+@app.route('/delete_profile', methods=['POST'])
+def delete_profile():
+    # Ottieni l'ID del profilo da eliminare dalla richiesta
+    data = request.json
+    profile_id = data['id']
+
+    # Nome del bucket e del file nel bucket
+    bucket_name = 'sitolamiere.appspot.com'
+    file_name = 'profili.json'
+    
+    # Inizializza il client di Google Cloud Storage
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    
+    # Leggi il contenuto esistente del file JSON dal bucket
+    json_data = json.loads(blob.download_as_string(client=None))
+    
+    # Ottieni la lista dei profili
+    profili = json_data.get('profili', [])
+    
+    # Rimuovi il profilo con l'ID specificato
+    profili = [profilo for profilo in profili if profilo['id'] != profile_id]
+    
+    # Aggiorna la lista dei profili nel dizionario json_data
+    json_data['profili'] = profili
+
+    # Sovrascrivi il file nel bucket con i nuovi dati
+    blob.upload_from_string(json.dumps(json_data, indent=4), content_type='application/json')
+    
+    return jsonify({'message': f'Profilo {profile_id} eliminato con successo!'})
+
+
+@app.route('/carica_pesi_materiali', methods=['GET'])
+def carica_pesi_materiali():
+    try:
+        # Carica tutti i dati dei materiali dal storage
+        dati_materiali = load_mats_from_storage()  # Assumi che questa funzione carichi i dati da Google Cloud Storage
+        print("dati materiali")
+        print(dati_materiali)
+        return jsonify(dati_materiali)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+
+@app.route('/save_mats', methods=['POST'])
+def save_mats():
+    # Carica i dati esistenti da Google Cloud Storage
+    data = load_mats_from_storage()
+   
+    print(data)  # Stampa i dati per debugging
+
+    # Supponendo che la richiesta POST invii i dati in JSON nel corpo della richiesta
+    request_data = request.get_json()
+    materiale = request_data.get('materiale')
+    tipologia = request_data.get('tipologia')
+    peso = request_data.get('peso')
+    spessore = request_data.get('spessore')
+    # Aggiungi il nuovo colore alla categoria corretta
+    if materiale not in data:
+            data[materiale] = [{
+            "peso": peso,
+            "spessore": [spessore],  # Supponendo che tu voglia aggiungere anche gli spessori in seguito
+            "tipologia": [tipologia]
+        }]
+    else:
+        return jsonify({'error': 'Materiale già esistente'}), 400
+    
+    # Salva i dati modificati su Google Cloud Storage
+    save_mats_to_storage(data)
+
+    return jsonify({'message': 'Colore aggiunto con successo'})
 
 if __name__ == '__main__':
     app.run(debug=True)
